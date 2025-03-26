243254e340f83d3b388a7dbc2d3c0a6497063950
Dxve7 <gracioli.dave@escola.pr.gov.br>
2025-03-26 13:51:19 +0000

    AULA 01 DE VARIAVEL

---
+++ src/main/java/com/mycompany/Aula01variáveis.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+public class Aula01variáveis {
+    public static void main(String[] args) {
+        // Variavel lógica
+
+        //Tipo primitivo
+        boolean valorLogicoPrimitivo = true;
+
+        //Tipo abstrato
+        boolean valorLogicoAbstrato = false;
+    }
+}
