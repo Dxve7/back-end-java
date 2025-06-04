c4dbecd872bf08b0a9cc3f194b06966d244e47b9
Dxve7 <gracioli.dave@escola.pr.gov.br>
2025-06-04 19:33:25 +0000

    aula08funções

--- src/main/java/com/mycompany/Aula08Funcoes.java
+++ src/main/java/com/mycompany/Aula08Funcoes.java
@@ -11,8 +11,31 @@ public class Aula08Funcoes {
 
         }
     }
+
+    /*
+     * Não posso declarar métodos dentro de outros métodos.
+     * 
+     * Toda função deve ter um returno, quando não há retorno utilizamos void.
+     * 
+     * Uma função pode não ter parâmetros, ou ter 1 ou mais.
+     */
     public static int multiplicacao(int num, int num2) {
         return num * num2;
     }
+    /*
+     * Funções recursivas
+     * 
+     * uma função recursiva faz uma chamada para ela própia
+     * 
+     * porém, toda recursividade deve ter um pomnto de parada,
+     * caso contrário pode resultar em um loop infinito
+     * 
+     */
+    public static int fatorial(int n) {
+        if (n != 0) {
+            return n * fatorial(n - 1);
+        } else {
+            return 1;
+        }
+    }
 }
-
