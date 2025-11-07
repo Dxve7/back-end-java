749328a0e51d99c898b97136b38f06f11865df7d
Dxve7 <gracioli.dave@escola.pr.gov.br>
2025-11-07 14:21:26 +0000

    aula13 carro veiculo

---
+++ src/main/java/com/mycompany/aula13/veiculos/Veiculo.java
@@ -0,0 +1,19 @@
+package com.mycompany.aula13.veiculos;
+
+
+    /*
+     * Interface tem como objetivo fornecer um "esboço ou molde"
+     * para serem implementados por outras classes
+     * 
+     * São muitos similares a classes abstratas, com a diferença que não possuem
+     * parâmetros e nem implementam métodos
+     */
+public interface Veiculo{
+
+    public void seguirEmFrente();
+
+    public void estacionar();
+
+    public void re();
+
+    }
